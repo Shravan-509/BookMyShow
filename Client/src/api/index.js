547d9e64fa5bd970d36f9f0fe1507b912c86db0b
Shravan Kumar <shravankumar.atti@gmail.com>
2025-07-04 02:17:12 +0530

    Changes to deploy on Netlify

--- Client/src/api/index.js
+++ Client/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 export const axiosInstance = axios.create({
-    baseURL: "/bms/v1",
+    baseURL: VITE_API_URL,
     headers: {
         "Content-Type" : "application/json"
     },
