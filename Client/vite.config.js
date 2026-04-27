702a029fa7d2e35000ee142139a539eddd00b880
Shravan Kumar <shravankumar.atti@gmail.com>
2026-04-27 22:22:37 +0530

    chore(build): stabilize vite config and dependencies
    - Simplified vite configuration
    - Removed unnecessary plugins and optimizations
    - Updated dependencies for compatibility
    - Regenerated package-lock.json after clean install
    - Fixed build and runtime issues
    - Downgraded to React 18 ecosystem
    - Updated AntD to v5 and Recharts to v2

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,17 +1,29 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  build: {
+    minify: 'esbuild', // faster + enough
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          antd: ['antd', '@ant-design/icons'],
+          vendor: ['react', 'react-dom', 'axios'],
+        }
+      }
+    }
+  },
+
   server: {
-    proxy :{
-      "/bms/v1" : {
-        target : "http://localhost:3000",
+    proxy: {
+      "/bms/v1": {
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false
-      },
-    },
-  },
+      }
+    }
+  }
 })
